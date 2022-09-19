30c7a5eaf092f3ce781597f7645d08bb19de9abe
Palmese Davide Mattia <107863855+PalmeseMattia@users.noreply.github.com>
2022-09-19 12:29:02 +0200

    Square by Bryan

---
+++ Square.java
@@ -0,0 +1,23 @@
+package Shape;
+
+public class Square extends Shape{
+
+    private double lato;
+    
+  Square(String colore, double raggio) {
+        super(colore);
+        this.lato = lato;
+    }
+
+    public double getPerimetro(){
+        return (this.lato * 4);
+    }
+
+    public double getArea(){
+        return (this.lato * this.lato);
+    }
+  
+    public double getLato(){
+        return this.lato;
+    }
+}
